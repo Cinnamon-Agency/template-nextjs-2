8db226714f25130ffab96bd0e5c459d6f08caf24
Martina Večerin <martina.vecerin@cinnamon.agency>
2022-11-25 14:14:05 +0100

    refactor: models, parameters and services

--- src/models/page.ts
+++ src/models/page.ts
@@ -1,8 +1,4 @@
 export interface MetaTagsModel {
-	url: string
 	title: string
 	description: string
-	keywords: string
-	image: string
-	type?: string
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,1 @@
-export { triggerEventGTM, getSource } from './ga'
-export { postEmail } from './email'
-export { postSubscribe } from './mailchimp'
-export { getBlog } from './blog'
 export { localAxios, remoteAxios } from './axios'
