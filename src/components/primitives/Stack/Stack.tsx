0ad920e98d4fc698e6c9926442fe6eeb531c584a
Karlo <kples96@gmail.com>
2023-01-13 11:07:31 +0100

    Add Stack layout primitive

---
+++ src/components/primitives/Stack/Stack.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+import { FlexFn, FlexProps, GapProp } from 'styles/theme'
+
+type Props = GapProp & Pick<FlexProps, 'direction' | 'align' | 'justify'>
+
+/**
+ * https://every-layout.dev/layouts/stack/
+ */
+export const Stack = styled.div<Props>`
+	flex-direction: column;
+	${FlexFn};
+`
