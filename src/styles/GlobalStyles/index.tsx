2bf41ed2128901a1b030283f7288833455b8974d
Karlo <kples96@gmail.com>
2023-01-13 11:28:36 +0100

    Bring back emotion-reset & emotion-normalize

--- src/styles/GlobalStyles/index.tsx
+++ src/styles/GlobalStyles/index.tsx
@@ -1,7 +1,16 @@
+import emotionReset from 'emotion-reset'
+import emotionNormalize from 'emotion-normalize'
 import { css, Global } from '@emotion/react'
 import { spacingCSSVars } from '../theme'
 
 const styles = css`
+	/* https://meyerweb.com/eric/tools/css/reset/ */
+	${emotionReset};
+
+	/* https://github.com/necolas/normalize.css/blob/master/normalize.css */
+	/* normalizer is setting Y-axis margins on <h1> only. Don't be confused */
+	${emotionNormalize};
+
 	* {
 		font-family: 'Inter', sans-serif;
 		box-sizing: border-box;
