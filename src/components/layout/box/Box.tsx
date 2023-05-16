8f5667cc6e12ad142eaf6bc7de10c6363f9e5970
Karlo <kples96@gmail.com>
2023-05-16 11:19:58 +0200

    Use util function to replace repeated code

--- src/components/layout/box/Box.tsx
+++ src/components/layout/box/Box.tsx
@@ -3,6 +3,7 @@ import classnames from 'classnames'
 import { ElementType, HTMLAttributes } from 'react'
 
 import { Atoms, atoms } from 'style/atoms.css'
+import { filterAtomsFromNativeAttributes } from 'style/utils/filterAtomsFromNativeAttributes'
 
 type Common<E extends ElementType> = HTMLAttributes<E> & { as?: E } & Atoms
 
@@ -12,18 +13,7 @@ type Props<E extends ElementType> = E extends keyof JSX.IntrinsicElements
 
 export const Box = <E extends ElementType = 'div'>({ as, className, ...rest }: Props<E>) => {
 	const Element = as || 'div'
-
-	const atomsProps: Record<string, unknown> = {}
-	const elementNativeProps: Record<string, unknown> = {}
-
-	// Filter out non-atoms props
-	Object.keys(rest).forEach(key => {
-		if (atoms.properties.has(key as keyof Atoms)) {
-			atomsProps[key] = rest[key as keyof typeof rest]
-		} else {
-			elementNativeProps[key] = rest[key as keyof typeof rest]
-		}
-	})
+	const { atomsProps, elementNativeProps } = filterAtomsFromNativeAttributes(rest)
 
 	return <Element className={classnames(atoms(atomsProps), className)} {...elementNativeProps} />
 }
