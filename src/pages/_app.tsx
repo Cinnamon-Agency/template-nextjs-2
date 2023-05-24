4b362acac765a062648c84f3a7df7a3de3e3bdff
Karlo <kples96@gmail.com>
2023-05-24 17:05:26 +0200

    Add next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import '../style/app.css'
+
+const inter = Inter({
+	weight: ['400', '600', '700'],
+	subsets: ['latin'],
+	variable: '--inter-font'
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />
+	return (
+		<div className={inter.className}>
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default App
