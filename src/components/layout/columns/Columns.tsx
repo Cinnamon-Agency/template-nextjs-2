36452744d017cdcdf24a93cca4c13af04b6d571c
Karlo <kples96@gmail.com>
2023-06-27 17:07:41 +0200

    Add example Columns component

---
+++ src/components/layout/columns/Columns.tsx
@@ -0,0 +1,26 @@
+// import { ReactNode } from 'react'
+//
+// export const Columns = ({ children, gap }: { children: ReactNode; gap?: any }) => null
+//
+// Columns.Item = ({ columns }: { columns: any; children: string }) => null
+//
+// const ExampleComponent = () => {
+// 	return (
+// 		<div>
+// 			<Columns>
+// 				<Columns.Item columns={6}>Something</Columns.Item>
+// 				<Columns.Item columns={6}>Something</Columns.Item>
+// 			</Columns>
+// 			<Columns gap="small">
+// 				<Columns.Item columns={{ mobile: 12, tablet: 6 }}>Something</Columns.Item>
+// 				<Columns.Item columns={{ mobile: 12, tablet: 6 }}>Something</Columns.Item>
+// 			</Columns>
+// 			<Columns>
+// 				<Columns.Item columns={12}>Something</Columns.Item>
+// 				<Columns.Item columns={6}>Something</Columns.Item>
+// 				<Columns.Item columns={6}>Something</Columns.Item>
+// 			</Columns>
+// 		</div>
+// 	)
+// }
+export {}
