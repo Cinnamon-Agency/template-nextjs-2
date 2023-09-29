eac32294d13c56c772421aa94e097cd42da2577e
Karlo <kples96@gmail.com>
2023-09-29 15:38:08 +0200

    Finish up documents UI

--- src/app/(dashboard)/people/add/documents.tsx
+++ src/app/(dashboard)/people/add/documents.tsx
@@ -1,13 +1,56 @@
 'use client'
 
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/data-display/accordion'
+import { AttachmentIcon } from '@/components/icons/attachment-icon'
+import { Button } from '@/components/inputs/button'
+import { Box } from '@/components/layout/box'
+import { Divider } from '@/components/layout/divider'
+import { Stack } from '@/components/layout/stack'
+import { Text } from '@/components/typography/text'
 
 export const Documents = () => {
 	return (
 		<Accordion type="single" collapsible>
 			<AccordionItem value="documents">
 				<AccordionTrigger>Documents</AccordionTrigger>
-				<AccordionContent>Accordion Content</AccordionContent>
+				<AccordionContent>
+					<Stack gap={8}>
+						<Divider />
+						<Box display="flex" gap={6}>
+							<div
+								style={{
+									width: '10rem',
+									flexShrink: 0,
+									display: 'flex',
+									justifyContent: 'flex-end',
+									alignItems: 'center'
+								}}>
+								<Text fontSize="small" fontWeight="semibold" as="label">
+									PK Kartica (optional)
+								</Text>
+							</div>
+							<div style={{ flexGrow: 1 }}>
+								<Button variant="secondary" size="small">
+									<AttachmentIcon size="small" />
+									Add an attachment
+								</Button>
+							</div>
+						</Box>
+						<Box display="flex" align="flex-start" gap={6}>
+							<div style={{ width: '10rem', flexShrink: 0, display: 'flex', textAlign: 'right' }}>
+								<Text fontSize="small" fontWeight="semibold" as="label">
+									Additional document (optional)
+								</Text>
+							</div>
+							<div style={{ flexGrow: 1 }}>
+								<Button variant="secondary" size="small">
+									<AttachmentIcon size="small" />
+									Add an attachment
+								</Button>
+							</div>
+						</Box>
+					</Stack>
+				</AccordionContent>
 			</AccordionItem>
 		</Accordion>
 	)
